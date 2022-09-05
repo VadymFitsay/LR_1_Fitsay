0794ed086ac270f9f465e584ed21dc92f2f36257
VadymFitsay <112889729+VadymFitsay@users.noreply.github.com>
2022-09-05 18:36:22 +0300

    Add files via upload

---
+++ JavaDoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"fib","l":"fib()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"fib","l":"fibo(int)"},{"p":"<Unnamed>","c":"fib","l":"geti()"},{"p":"<Unnamed>","c":"fib","l":"i"},{"p":"<Unnamed>","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
